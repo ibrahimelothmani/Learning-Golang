661d3e020f6d5b84c0074945ba4f0706edf1da70
Ibrahim <ibrahimelothmani98@gmail.com>
2025-06-21 07:12:25 -1200

    feat: enhance Investment function to accept user input for parameters

--- investment.go
+++ investment.go
@@ -8,9 +8,23 @@ import (
 
 func Investment() {
 
-	var investmentAmount float64 = 1000 // Example investment amount
-	var expectedReturnRate float64 = 5.5 // Example expected return rate in percentage
-	var years int = 10 // Example investment duration in years
+	// const inflationRate = 6.5 // Example inflation rate in percentage
+	// var investmentAmount float64 = 1000 // Example investment amount
+	// var expectedReturnRate float64 = 5.5 // Example expected return rate in percentage
+	// var years int = 10 // Example investment duration in years
+	var inflationRate, investmentAmount, expectedReturnRate float64
+	var years int
+	fmt.Print("Enter the Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter the Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Enter the InflationRate: ")
+	fmt.Scan(&inflationRate)
+	fmt.Print("Enter the Number of Years: ")
+	fmt.Scan(&years)
+	
 	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
 	fmt.Println("Future Value of Investment: ", futureValue)
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), float64(years))
+	fmt.Printf("Future Real Value of Investment (adjusted for inflation): %.2f\n", futureRealValue)
 }
