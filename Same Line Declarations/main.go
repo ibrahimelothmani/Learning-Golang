621c73fade6854a1c576df245a89241f3d757c3d
Ibrahim <ibrahimelothmani98@gmail.com>
2025-06-28 01:06:29 -1200

    feat: how to declare multiple variables on the same line in Go

---
+++ Same Line Declarations/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	averageOpenRate, displayMessage := 0.23, "is the average open rate of your messages"
+	fmt.Println(averageOpenRate, displayMessage)
+}
+// This code demonstrates how to declare multiple variables on the same line in Go.
