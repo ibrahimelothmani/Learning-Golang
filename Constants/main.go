7d498a33f7f3e70b4b837b5a34917df331a21f2a
Ibrahim <ibrahimelothmani98@gmail.com>
2025-06-28 01:16:20 -1200

    chore: remove unused .idea configuration files and add main.go for constants demonstration

---
+++ Constants/main.go
@@ -0,0 +1,17 @@
+// Constants are declared with the const keyword. They can't use the := short declaration syntax.
+// const pi = 3.14159
+
+package main
+
+import "fmt"
+
+func main() {
+	const premiumPlanName = "Premium Plan"
+	basicPlanName := "Basic Plan"
+
+	// don't edit below this line
+
+	fmt.Println("plan:", premiumPlanName)
+	fmt.Println("plan:", basicPlanName)
+}
+// This code demonstrates how to declare constants and variables in Go.
