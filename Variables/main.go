acc9f78360fa04713039efd2f1b5566e82d96a2a
Ibrahim <ibrahimelothmani98@gmail.com>
2025-06-27 21:34:02 -1200

    feat: update message length information in output

--- Variables/main.go
+++ Variables/main.go
@@ -7,4 +7,10 @@ func main() {
 	costPerMessage := .02
 	totalCost := costPerMessage * float64(numMessagesFromDoris)
 	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
+
+	fmt.Printf("######################################################\n")
+
+	maxMessageLength := 140
+	newMaxMessageLength := 280
+	fmt.Println("text is increasing the maximum message length from", maxMessageLength, "to", newMaxMessageLength, "characters.")
 }
